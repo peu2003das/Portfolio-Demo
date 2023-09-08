fb30f60d0d28a49d4213d43a6256cb9e91300052
Sanjana Das <peud438@gmail.com>
2023-09-08 17:28:32 +0530

    Fix Css And JS

--- html/script.js
+++ html/script.js
@@ -1,15 +1,15 @@
 console.log("Script running...")
-document.querySelector('.cross').style.display = 'none' ;
+// document.querySelector('.cross').style.display = 'none' ;
 document.querySelector('.hamburger').addEventListener("click", ()=>{
     document.querySelector('.sidebar').classList.toggle('sidebarGo');
     if( document.querySelector('.sidebar').classList.contains('sidebarGo')){
         document.querySelector('.ham').style.disply = 'inline'
-        document.querySelector('.cross').style.disply = 'none'
+        // document.querySelector('.cross').style.disply = 'none'
     }
     else{
         document.querySelector('.ham').style.disply = 'none'
         setTimeout(() => {
-        document.querySelector('.cross').style.disply = 'inline'
+        // document.querySelector('.cross').style.disply = 'inline'
         }, 300);
     }
 
